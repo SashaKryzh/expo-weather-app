b1f6e483a5a29be96c530591222ec0a227131164
Oleksandr Kryzhanovskyi <sashakryzh@gmail.com>
2023-10-29 20:09:46 +0100

    fixed font

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,25 @@
+import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
+import { useEffect } from 'react';
 import { TamaguiProvider } from 'tamagui';
 import config from '../tamagui.config';
 
 export default function Layout() {
+  const [loaded] = useFonts({
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      // can hide splash screen here
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <TamaguiProvider config={config}>
       <Stack />
